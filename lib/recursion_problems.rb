678c2a838266a5ddcf65dbd13417ffd1fedc1a96
metacoder87 <gary.miller@gmx.com>
2020-10-18 15:56:21 -0400

    Adds in the solution for sorted?

--- lib/recursion_problems.rb
+++ lib/recursion_problems.rb
@@ -1,4 +1,4 @@
-#Problem 1: 
+p "Problem 1:"
 
 def sum_recur(array)
     return array.first if array.count == 1
@@ -9,7 +9,7 @@ p sum_recur([1,2,3]) # => 6
 p sum_recur([1,2,3,4]) # => 10
 p sum_recur([1,2,3,4,5]) # => 15
 
-#Problem 2: 
+ p "Problem 2:" 
 
 def includes?(array, target)
     return false if array.empty?
@@ -20,7 +20,7 @@ p includes?([1,2,3,4,5], 4) # => true
 p includes?([1,2,3,4,5], 3) # => true
 p includes?([1,2,3,4,5], 8) # => false
 
-# Problem 3:  
+p "Problem 3:"  
 
 def num_occur(array, target, count = 0)
     return count if array.count == 0
@@ -31,7 +31,7 @@ p num_occur([1,2,3,4,5], 2) # => 1
 p num_occur([1,2,2,3,4], 2) # => 2
 p num_occur([1,3,4,5], 2) # => 0
 
-# Problem 4: 
+p "Problem 4:" 
 
 def add_to_twelve?(array)
     return false if array.length == 1 && array[0] != 12
@@ -45,11 +45,20 @@ p add_to_twelve?([8]) # => false
 p add_to_twelve?([8,4]) # => true
 p add_to_twelve?([12])  # => true
 
-# Problem 5: 
+p "Problem 5:" 
 
 def sorted?(array)
-    
+    return true if array.count < 2
+    return array[0..1].sort == array[0..1] if array.length == 2
+    array[0..1].sort != array[0..1] ? false : sorted?(array.drop(1))
 end
+
+p sorted?([1,2]) # => true
+p sorted?([1,2,3]) # => true
+p sorted?([1,2,3,4]) # => true
+p sorted?([1,2,3,4,5]) # => true
+p sorted?([1,8,2,3]) # => false
+p sorted?([1,0,3]) # => false
 
 # Problem 6: 
 
